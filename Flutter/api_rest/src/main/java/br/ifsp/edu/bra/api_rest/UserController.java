c55137deff3460e2c16beea456e49be019e18794
Tsarco <wedontexist777@gmail.com>
2026-03-16 01:00:55 -0300

    alteracoes add

--- Flutter/api_rest/src/main/java/br/ifsp/edu/bra/api_rest/UserController.java
+++ Flutter/api_rest/src/main/java/br/ifsp/edu/bra/api_rest/UserController.java
@@ -3,6 +3,7 @@ package br.ifsp.edu.bra.api_rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 // Import automático: shift + alt + o
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/api")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @PostMapping("user")
     public ResponseEntity<User> createUser(@RequestBody User user) {
