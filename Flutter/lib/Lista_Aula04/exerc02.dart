474b2a91522cabd838ce61ec0ba4d20945724538
Tsarco <wedontexist777@gmail.com>
2026-03-28 18:17:42 -0300

    exerc02 feito

---
+++ Flutter/lib/Lista_Aula04/exerc02.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.blue),
+
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: Theme.of(context).copyWith(scaffoldBackgroundColor: Colors.yellow),
+
+      child: Scaffold(
+        appBar: AppBar(title: Text("Exerc01 com Theme Data")),
+
+        body: Center(child: Text("Texto")),
+      ),
+    );
+  }
+}
